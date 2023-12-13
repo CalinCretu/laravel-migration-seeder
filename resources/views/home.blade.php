92c32a250aa17eb078702b8ba3e92dd005beac3a
CalinCretu <calincretu1412@gmail.com>
2023-12-13 15:51:59 +0100

    ADD - home_page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('content')
+    <section>
+        <div class="container d-flex justify-content-center main-section">
+            <h1>
+                Home Content
+            </h1>
+        </div>
+    </section>
+@endsection
